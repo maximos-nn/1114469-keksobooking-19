826f20aa50fdbb47ac7d8afe102587335e9b6ddd
maximos-nn <52293321+maximos-nn@users.noreply.github.com>
2020-02-17 23:11:00 +0300

    Перламутровые пуговицы (часть 2)

--- js/filter.js
+++ js/filter.js
@@ -2,15 +2,27 @@
 
 (function () {
   var filters = document.querySelector('.map__filters');
-  var housingType = filters.querySelector('#housing-type');
-  var housingPrice = filters.querySelector('#housing-price');
   var subscriber;
 
-  function onFilterFormChange() {
+  function getFilterData() {
+    var housingType = filters.querySelector('#housing-type');
+    var housingPrice = filters.querySelector('#housing-price');
+    var housingRooms = filters.querySelector('#housing-rooms');
+    var housingGuests = filters.querySelector('#housing-guests');
+    var housingFeatures = filters.querySelectorAll('#housing-features input:checked');
     var filterData = {};
     filterData[window.data.FilterType.HOUSING_TYPE] = housingType.value;
     filterData[window.data.FilterType.HOUSING_PRICE] = housingPrice.value;
-    window.data.setFilter(filterData);
+    filterData[window.data.FilterType.HOUSING_ROOMS] = housingRooms.value;
+    filterData[window.data.FilterType.HOUSING_GUESTS] = housingGuests.value;
+    filterData[window.data.FilterType.HOUSING_FEATURES] = Array.from(housingFeatures).map(function (feature) {
+      return feature.value;
+    });
+    return filterData;
+  }
+
+  function onFilterFormChange() {
+    window.data.setFilter(getFilterData());
     if (typeof subscriber === 'function') {
       subscriber();
     }
