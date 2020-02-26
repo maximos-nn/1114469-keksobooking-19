98a05d7784726536b20b4781eab90fd08ff084f2
maximos-nn <52293321+maximos-nn@users.noreply.github.com>
2020-02-26 10:42:15 +0300

    Сброс удобств в форме фильтрации

--- js/filter.js
+++ js/filter.js
@@ -2,20 +2,22 @@
 
 (function () {
   var filters = document.querySelector('.map__filters');
+  var filterControls = filters.querySelectorAll('select, fieldset');
   var housingType = filters.querySelector('#housing-type');
   var housingPrice = filters.querySelector('#housing-price');
   var housingRooms = filters.querySelector('#housing-rooms');
   var housingGuests = filters.querySelector('#housing-guests');
+  var housingFeatures = filters.querySelector('#housing-features');
   var subscriber;
 
   function getFilterData() {
-    var housingFeatures = filters.querySelectorAll('#housing-features input:checked');
+    var checkedHousingFeatures = housingFeatures.querySelectorAll('input:checked');
     var filterData = {};
     filterData[window.data.FilterType.HOUSING_TYPE] = housingType.value;
     filterData[window.data.FilterType.HOUSING_PRICE] = housingPrice.value;
     filterData[window.data.FilterType.HOUSING_ROOMS] = housingRooms.value;
     filterData[window.data.FilterType.HOUSING_GUESTS] = housingGuests.value;
-    filterData[window.data.FilterType.HOUSING_FEATURES] = Array.from(housingFeatures).map(function (feature) {
+    filterData[window.data.FilterType.HOUSING_FEATURES] = Array.from(checkedHousingFeatures).map(function (feature) {
       return feature.value;
     });
     return filterData;
@@ -29,10 +31,14 @@
   }
 
   function toggleFilters(active) {
-    var controls = filters.querySelectorAll('select, fieldset');
-    controls.forEach(function (control) {
+    filterControls.forEach(function (control) {
       control.disabled = !active;
     });
+    if (!active) {
+      housingFeatures.querySelectorAll('input:checked').forEach(function (control) {
+        control.checked = false;
+      });
+    }
   }
 
   function setFilterChangeCb(callback) {
