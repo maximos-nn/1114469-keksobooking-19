fce9935b1c64674ec7e5124306904a66793cc567
maximos-nn <52293321+maximos-nn@users.noreply.github.com>
2020-03-01 20:04:57 +0300

    Модуль для главного пина

--- js/map.js
+++ js/map.js
@@ -3,49 +3,17 @@
 (function () {
   var PIN_OFFSET_X = -25;
   var PIN_OFFSET_Y = -70;
-  var PIN_NIB_HEIGHT = 16;
-  var PIN_MIN_Y = 130;
-  var PIN_MAX_Y = 630;
-  var PIN_MIN_X = 0;
-  var MAIN_PIN_DEFAULT_LEFT = '570px';
-  var MAIN_PIN_DEFAULT_TOP = '375px';
 
   var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
   var map = document.querySelector('.map');
   var mapPins = map.querySelector('.map__pins');
-  var customPin = mapPins.querySelector('.map__pin--main');
-  var isActive = false;
-  var pinMaxX = mapPins.offsetWidth - 1;
   var dataLoadedSubscribes = [];
-  var pinPositionChangeSubscribes = [];
-  var posConstraints = new window.position.PositionConstraints(PIN_MIN_X, PIN_MIN_Y, pinMaxX, PIN_MAX_Y);
-
-  function callPinPosChangeHandlers() {
-    var address = getCustomPinAddress();
-    pinPositionChangeSubscribes.forEach(function (handler) {
-      handler(address);
-    });
-  }
 
   function setPinActive(currentPin) {
     mapPins.querySelectorAll('.map__pin:not(.map__pin--main)').forEach(function (pin) {
       pin.classList.remove('map__pin--active');
     });
     currentPin.classList.add('map__pin--active');
-  }
-
-  function clear() {
-    window.card.close();
-    mapPins.querySelectorAll('.map__pin:not(.map__pin--main)').forEach(function (pin) {
-      pin.remove();
-    });
-  }
-
-  function reset() {
-    clear();
-    customPin.style.left = MAIN_PIN_DEFAULT_LEFT;
-    customPin.style.top = MAIN_PIN_DEFAULT_TOP;
-    callPinPosChangeHandlers();
   }
 
   function createPin(advert) {
@@ -74,13 +42,20 @@
     });
   }
 
-  function onFilterFormChange() {
+  function clear() {
+    window.card.close();
+    mapPins.querySelectorAll('.map__pin:not(.map__pin--main)').forEach(function (pin) {
+      pin.remove();
+    });
+  }
+
+  function reset() {
     clear();
-    renderPins(mapPins, window.data.getAdverts());
+    window.mapCustomPin.reset();
   }
 
   function toggle(active) {
-    isActive = active;
+    window.mapCustomPin.toggle(active);
     if (active) {
       map.classList.remove('map--faded');
       renderPins(mapPins, window.data.getAdverts());
@@ -93,24 +68,19 @@
     window.filter.toggle(active);
   }
 
-  function getCustomPinCoords() {
-    var height = customPin.offsetHeight;
-    var x = Math.floor(customPin.offsetLeft + customPin.offsetWidth / 2);
-    if (!isActive) {
-      return new window.position.Position(x, Math.floor(customPin.offsetTop + height / 2));
-    }
-    return new window.position.Position(x, Math.floor(customPin.offsetTop + height + PIN_NIB_HEIGHT));
-  }
-
-  function getCustomPinAddress() {
-    var coords = getCustomPinCoords();
-    return coords.x + ', ' + coords.y;
-  }
-
   function setDataLoadedCb(callback) {
     if (typeof callback === 'function') {
       dataLoadedSubscribes.push(callback);
     }
+  }
+
+  function setPinPositionChangeCb(callback) {
+    window.mapCustomPin.setPositionChangeCb(callback);
+  }
+
+  function onFilterFormChange() {
+    clear();
+    renderPins(mapPins, window.data.getAdverts());
   }
 
   function onDataLoaded() {
@@ -119,42 +89,12 @@
     });
   }
 
-  function onCustomPinMousedown(evt) {
-    if (evt.button === window.utils.Const.MAIN_MOUSE_BUTTON && !isActive) {
-      window.data.loadAdverts(onDataLoaded);
-    }
+  function onCustomPinProceed() {
+    window.data.loadAdverts(onDataLoaded);
   }
 
-  function onCustomPinEnterKey(evt) {
-    if (evt.key === window.utils.Const.ENTER_KEY && !isActive) {
-      window.data.loadAdverts(onDataLoaded);
-    }
-  }
-
-  function setCustomPinPosition(shift) {
-    var oldPos = getCustomPinCoords();
-    var newValidPos = new window.position.Position(oldPos.x + shift.x, oldPos.y + shift.y, posConstraints);
-    var diff = oldPos.getShift(newValidPos);
-
-    customPin.style.top = customPin.offsetTop + diff.y + 'px';
-    customPin.style.left = customPin.offsetLeft + diff.x + 'px';
-  }
-
-  function onCustomPinDrag(shift) {
-    setCustomPinPosition(shift);
-    callPinPosChangeHandlers();
-  }
-
-  function setPinPositionChangeCb(callback) {
-    if (typeof callback === 'function') {
-      pinPositionChangeSubscribes.push(callback);
-    }
-  }
-
-  customPin.addEventListener('mousedown', onCustomPinMousedown);
-  customPin.addEventListener('keydown', onCustomPinEnterKey);
-  customPin.addEventListener('mousedown', window.drag.onMouseDown);
-  window.drag.setMouseMoveCb(onCustomPinDrag);
+  window.mapCustomPin.init(mapPins);
+  window.mapCustomPin.setProceedCb(onCustomPinProceed);
 
   window.map = {
     toggle: toggle,
